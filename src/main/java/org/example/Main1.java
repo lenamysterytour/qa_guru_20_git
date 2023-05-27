50f385f2b69eb580cd0b2e1c3246a67f82a98b24
Bolotnikova Elena <lenamysterytour@gmail.com>
2023-05-27 11:26:36 +0300

    change logic valera

--- src/main/java/org/example/Main1.java
+++ src/main/java/org/example/Main1.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Main1 {
-    public static void main(String[] args) {if (args.length ==0){
+    public static void main(String[] args) {if (args.length ==1){
         String message = "Hello gq guru&&&!";
         System.out.println(message);
     }
